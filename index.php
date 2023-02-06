25cf15900bf7f4e7507fab358da1601d350b0bd9
Robin Hodge <mrrobinhodge@gmail.com>
2023-02-06 15:25:23 +0000

    The index file has the basic HTML structure that I have decided to go for for the applicaiton. This includes sections for each of the fields that exist in the database.

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+?>
+
+<!DOCTYPE HTML>
+<HTML>
+    <head>
+        <link href='styles.css' rel="stylesheet" type='text/css' />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    </head>
+    <body>
+        <h1 id="page title">Your Dream Diary</h1>
+        <h3 id="dream title">Walls in house made of sponge cake!</h3>
+        <h4 id="dream or nightmare">Dream/Nightmare</h4>
+        <p id="dream description">I had a dream where I saw cracks appearing in the walls of my house, 
+            that started to grow and grow. As they grew, they started to look like sponge cake,
+             and fell away to reveal huge holes in the wall. How odd!!
+        </p>
+        <h1 id="page title">Your Dream Diary</h1>
+        <h3 id="dream title">Walls in house made of sponge cake!</h3>
+        <h4 id="dream or nightmare">Dream/Nightmare</h4>
+        <p id="dream description">I had a dream where I saw cracks appearing in the walls of my house, 
+            that started to grow and grow. As they grew, they started to look like sponge cake,
+             and fell away to reveal huge holes in the wall. How odd!!
+        </p>
+        <h1 id="page title">Your Dream Diary</h1>
+        <h3 id="dream title">Walls in house made of sponge cake!</h3>
+        <h4 id="dream or nightmare">Dream/Nightmare</h4>
+        <p id="dream description">I had a dream where I saw cracks appearing in the walls of my house, 
+            that started to grow and grow. As they grew, they started to look like sponge cake,
+             and fell away to reveal huge holes in the wall. How odd!!
+        </p>
+        <h1 id="page title">Your Dream Diary</h1>
+        <h3 id="dream title">Walls in house made of sponge cake!</h3>
+        <h4 id="dream or nightmare">Dream/Nightmare</h4>
+        <p id="dream description">I had a dream where I saw cracks appearing in the walls of my house, 
+            that started to grow and grow. As they grew, they started to look like sponge cake,
+             and fell away to reveal huge holes in the wall. How odd!!
+        </p>
+    </body>
+</HTML>
