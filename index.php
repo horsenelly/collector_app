0a9cb46ffa1f7ff720aeb1e08ea89833096e893d
Robin Hodge <mrrobinhodge@gmail.com>
2023-02-06 15:57:26 +0000

    Basic html structure done. Odd result with styles.css not linking reliably is being ignored for now!

--- index.php
+++ index.php
@@ -4,37 +4,48 @@
 <!DOCTYPE HTML>
 <HTML>
     <head>
-        <link href='styles.css' rel="stylesheet" type='text/css' />
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+        <link rel="stylesheet" type="text/css" href="/styles/styles.css" >
     </head>
     <body>
         <h1 id="page title">Your Dream Diary</h1>
+        <h2 id="page sub-heading">Dreams you have had</h2>
         <h3 id="dream title">Walls in house made of sponge cake!</h3>
         <h4 id="dream or nightmare">Dream/Nightmare</h4>
+        <h4>01/01/1970</h4>
         <p id="dream description">I had a dream where I saw cracks appearing in the walls of my house, 
             that started to grow and grow. As they grew, they started to look like sponge cake,
-             and fell away to reveal huge holes in the wall. How odd!!
+             and fell away to reveal huge holes in the wall. How odd!! I spoke with my therapist about this,
+             and she said it might be a metaphore about all of the things that I am learning, and feeling like 
+             my brain almost doesn't have space to store it all! I'm not toally sure I agree with this, but who 
+             knows, she is the pro....
         </p>
-        <h1 id="page title">Your Dream Diary</h1>
         <h3 id="dream title">Walls in house made of sponge cake!</h3>
         <h4 id="dream or nightmare">Dream/Nightmare</h4>
+        <h4>01/01/1970</h4>
         <p id="dream description">I had a dream where I saw cracks appearing in the walls of my house, 
             that started to grow and grow. As they grew, they started to look like sponge cake,
-             and fell away to reveal huge holes in the wall. How odd!!
-        </p>
-        <h1 id="page title">Your Dream Diary</h1>
+             and fell away to reveal huge holes in the wall. How odd!! I spoke with my therapist about this,
+             and she said it might be a metaphore about all of the things that I am learning, and feeling like 
+             my brain almost doesn't have space to store it all! I'm not toally sure I agree with this, but who 
+             knows, she is the pro....
         <h3 id="dream title">Walls in house made of sponge cake!</h3>
         <h4 id="dream or nightmare">Dream/Nightmare</h4>
+        <h4>01/01/1970</h4>
         <p id="dream description">I had a dream where I saw cracks appearing in the walls of my house, 
             that started to grow and grow. As they grew, they started to look like sponge cake,
-             and fell away to reveal huge holes in the wall. How odd!!
-        </p>
-        <h1 id="page title">Your Dream Diary</h1>
+             and fell away to reveal huge holes in the wall. How odd!! I spoke with my therapist about this,
+             and she said it might be a metaphore about all of the things that I am learning, and feeling like 
+             my brain almost doesn't have space to store it all! I'm not toally sure I agree with this, but who 
+             knows, she is the pro....
         <h3 id="dream title">Walls in house made of sponge cake!</h3>
         <h4 id="dream or nightmare">Dream/Nightmare</h4>
+        <h4>01/01/1970</h4>
         <p id="dream description">I had a dream where I saw cracks appearing in the walls of my house, 
             that started to grow and grow. As they grew, they started to look like sponge cake,
-             and fell away to reveal huge holes in the wall. How odd!!
-        </p>
+             and fell away to reveal huge holes in the wall. How odd!! I spoke with my therapist about this,
+             and she said it might be a metaphore about all of the things that I am learning, and feeling like 
+             my brain almost doesn't have space to store it all! I'm not toally sure I agree with this, but who 
+             knows, she is the pro....
     </body>
 </HTML>
