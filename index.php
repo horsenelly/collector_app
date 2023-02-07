04e28ddf55ea1b9442421a577a01de7e9281efc0
Robin Hodge <mrrobinhodge@gmail.com>
2023-02-07 10:31:22 +0000

    This has a working version of the db being output into the html page.

--- index.php
+++ index.php
@@ -1,30 +1,22 @@
 <?php
+require 'src/functions.php';
+
+$dbString = 'dreams';
+$db = connectToDB($dbString);
+$dreams = getAllDreams($db);
+$displayedDreams = displayDreamDetails($dreams);
+
 ?>
 
 <!DOCTYPE HTML>
 <HTML>
 <head>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-     <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
 </head>
 <body>
     <h1>Your Dream Diary</h1>
-    <h2>Dreams you have had</h2>
-    <h3>Walls in house made of sponge cake!</h3>
-    <h4>Dream/Nightmare</h4>
-    <h4>01/01/1970</h4>
-    <p>I had a dream where I saw cracks appearing in the walls of my house, that started to grow and grow. As they grew, they started to look like sponge cake,and fell away to reveal huge holes in the wall. How odd!! I spoke with my therapist about this,and she said it might be a metaphore about all of the things that I am learning, and feeling like my brain almost doesn't have space to store it all! I'm not toally sure I agree with this, but who knows, she is the pro....</p>
-    <h3>Walls in house made of sponge cake!</h3>
-    <h4>Dream/Nightmare</h4>
-    <h4>01/01/1970</h4>
-    <p">I had a dream where I saw cracks appearing in the walls of my house, that started to grow and grow. As they grew, they started to look like sponge cake,and fell away to reveal huge holes in the wall. How odd!! I spoke with my therapist about this,and she said it might be a metaphore about all of the things that I am learning, and feeling like my brain almost doesn't have space to store it all! I'm not toally sure I agree with this, but who knows, she is the pro....</p>
-    <h3">Walls in house made of sponge cake!</h3>
-    <h4>Dream/Nightmare</h4>
-    <h4>01/01/1970</h4>
-    <p>I had a dream where I saw cracks appearing in the walls of my house, that started to grow and grow. As they grew, they started to look like sponge cake,and fell away to reveal huge holes in the wall. How odd!! I spoke with my therapist about this,and she said it might be a metaphore about all of the things that I am learning, and feeling like my brain almost doesn't have space to store it all! I'm not toally sure I agree with this, but who knows, she is the pro....</p>
-    <h3">Walls in house made of sponge cake!</h3>
-    <h4>Dream/Nightmare</h4>
-    <h4>01/01/1970</h4>
-    <p>I had a dream where I saw cracks appearing in the walls of my house, that started to grow and grow. As they grew, they started to look like sponge cake,and fell away to reveal huge holes in the wall. How odd!! I spoke with my therapist about this,and she said it might be a metaphore about all of the things that I am learning, and feeling like my brain almost doesn't have space to store it all! I'm not toally sure I agree with this, but who knows, she is the pro....</p>
+    <h2>Your dreams</h2>
+    <?php echo $displayedDreams; ?>
 </body>
 </HTML>
--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function connectToDB(string$dbName): PDO {
+function connectToDB(string $dbName): PDO {
     $connectionString ="mysql:host=db; dbname=$dbName";
     $db = new PDO($connectionString, 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
@@ -8,17 +8,18 @@ function connectToDB(string$dbName): PDO {
 }
 
 function getAllDreams(PDO $db): array {
-    $query = $db->prepare(SELECT * FROM dreams);
-    $query->execute;
+    $query = $db->prepare('SELECT * FROM dreams;');
+    $query->execute();
     return  $query->fetchALL();
 }
 
-function displayDreamDetails(array $dreams){
-    foreach($dreams as $dream);
-    return "<h2>Dreams you have had</h2>
-    <h3>" . $dream['dream_title'] . "</h3>
-    <h4>" . $dream['dream_or_nightmare'] . "</h4>
-    <h4>" . $dream['date'] . "</h4>
-    <p>" . $dream['dream_description'] . 
-    "</p>"
+function displayDreamDetails(array $dreams): string{
+    $dreamOutput ='';
+    foreach($dreams as $dream){
+        $dreamOutput .="<h3>" . $dream['dream_title'] . "</h3>
+        <h4>" . $dream['dream_or_nightmare'] . "</h4>
+        <h4>" . $dream['dream_date'] . "</h4>
+        <p>" . $dream['dream_description'] . 
+        "</p>";
+    } return $dreamOutput;
 }
